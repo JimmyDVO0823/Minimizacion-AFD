084aada0ebcca632c3d7b1e7ed85214e8e2662b6
JimmyDVO0823 <jimmyvelandia@outlook.es>
2025-11-10 14:25:54 -0500

    MVC

--- src/main/java/Minimizacion/Model/AFD.java
+++ src/main/java/Minimizacion/Model/AFD.java
@@ -2,11 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Minimizacion;
+package Minimizacion.Model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
